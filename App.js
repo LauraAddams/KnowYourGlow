61d2be57bc2790d8324780389450c682431afcbf
Laura Robertson <mslaura.mae@gmail.com>
2018-01-06 23:26:25 -0800

    Expo splash screen(mayBEE)

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { Tabs } from './config/router';
+import { Asset, AppLoading } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    isReady: false,
+  };
+
+  async _cacheResourcesAsync() {
+    return true;
+  }
+
   render() {
+    if(!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+          />
+      );
+    }
     return <Tabs />
   }
 }
